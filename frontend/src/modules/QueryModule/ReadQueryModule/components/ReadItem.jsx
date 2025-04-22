274769963c5dff127684ce62b937194cc701e688
akashk11 <akashvinod777@gmail.com>
2025-04-22 17:51:42 +0400

    add ReadQueryModule and ReadItem components for displaying query details

---
+++ frontend/src/modules/QueryModule/ReadQueryModule/components/ReadItem.jsx
@@ -0,0 +1,207 @@
+import { useState, useEffect } from 'react';
+import { Divider } from 'antd';
+
+import { Button, Row, Col, Descriptions, Statistic, Tag } from 'antd';
+import { PageHeader } from '@ant-design/pro-layout';
+import {
+  EditOutlined,
+  FilePdfOutlined,
+  CloseCircleOutlined,
+  RetweetOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+
+import { useSelector, useDispatch } from 'react-redux';
+import useLanguage from '@/locale/useLanguage';
+import { erp } from '@/redux/erp/actions';
+
+import { generate as uniqueId } from 'shortid';
+
+import { selectCurrentItem } from '@/redux/erp/selectors';
+
+import { DOWNLOAD_BASE_URL } from '@/config/serverApiConfig';
+import { useMoney, useDate } from '@/settings';
+import useMail from '@/hooks/useMail';
+import { useNavigate } from 'react-router-dom';
+
+const Item = ({ item, currentErp }) => {
+  const { moneyFormatter } = useMoney();
+  return (
+    <Row gutter={[12, 0]} key={item._id}>
+      <Col className="gutter-row" span={11}>
+        <p style={{ marginBottom: 5 }}>
+          <strong>{item.text}</strong>
+        </p>
+      </Col>
+
+      <Divider dashed style={{ marginTop: 0, marginBottom: 5 }} />
+    </Row>
+  );
+};
+
+export default function ReadItem({ config, selectedItem }) {
+  const translate = useLanguage();
+  const { entity, ENTITY_NAME } = config;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { moneyFormatter } = useMoney();
+  const { send, isLoading: mailInProgress } = useMail({ entity });
+
+  const { result: currentResult } = useSelector(selectCurrentItem);
+
+  const resetErp = {
+    status: '',
+    client: {
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+    },
+    subTotal: 0,
+    taxTotal: 0,
+    taxRate: 0,
+    total: 0,
+    credit: 0,
+    number: 0,
+    year: 0,
+  };
+
+  const [notes, setNotes] = useState([]);
+  const [currentErp, setCurrentErp] = useState(selectedItem ?? resetErp);
+  const [client, setClient] = useState({});
+
+  useEffect(() => {
+    if (currentResult) {
+      const { notes, invoice, ...others } = currentResult;
+
+      if (notes) {
+        setNotes(notes);
+        setCurrentErp(currentResult);
+      } else if (invoice?.notes) {
+        setNotes(invoice.notes);
+        setCurrentErp({ ...invoice.notes, ...others, ...invoice });
+      }
+    }
+    return () => {
+      setNotes([]);
+      setCurrentErp(resetErp);
+    };
+  }, [currentResult]);
+
+  useEffect(() => {
+    if (currentErp?.client) {
+      setClient(currentErp.client);
+    }
+  }, [currentErp]);
+
+  return (
+    <>
+      <PageHeader
+        onBack={() => {
+          navigate(`/${entity.toLowerCase()}`);
+        }}
+        title={`${ENTITY_NAME} # ${currentErp.number}`}
+        ghost={false}
+        tags={[
+          <span key="status">{currentErp.status && translate(currentErp.status)}</span>,
+          currentErp.paymentStatus && (
+            <span key="paymentStatus">
+              {currentErp.paymentStatus && translate(currentErp.paymentStatus)}
+            </span>
+          ),
+        ]}
+        extra={[
+          <Button
+            key={`${uniqueId()}`}
+            onClick={() => {
+              navigate(`/${entity.toLowerCase()}`);
+            }}
+            icon={<CloseCircleOutlined />}
+          >
+            {translate('Close')}
+          </Button>,
+          <Button
+            key={`${uniqueId()}`}
+            onClick={() => {
+              window.open(
+                `${DOWNLOAD_BASE_URL}${entity}/${entity}-${currentErp._id}.pdf`,
+                '_blank'
+              );
+            }}
+            icon={<FilePdfOutlined />}
+          >
+            {translate('Download PDF')}
+          </Button>,
+          <Button
+            key={`${uniqueId()}`}
+            loading={mailInProgress}
+            onClick={() => {
+              send(currentErp._id);
+            }}
+            icon={<MailOutlined />}
+          >
+            {translate('Send by Email')}
+          </Button>,
+          <Button
+            key={`${uniqueId()}`}
+            onClick={() => {
+              dispatch(erp.convert({ entity, id: currentErp._id }));
+            }}
+            icon={<RetweetOutlined />}
+            style={{ display: entity === 'quote' ? 'inline-block' : 'none' }}
+          >
+            {translate('Convert to Invoice')}
+          </Button>,
+
+          <Button
+            key={`${uniqueId()}`}
+            onClick={() => {
+              dispatch(
+                erp.currentAction({
+                  actionType: 'update',
+                  data: currentErp,
+                })
+              );
+              navigate(`/${entity.toLowerCase()}/update/${currentErp._id}`);
+            }}
+            type="primary"
+            icon={<EditOutlined />}
+          >
+            {translate('Edit')}
+          </Button>,
+        ]}
+        style={{
+          padding: '20px 0px',
+        }}
+      >
+        <Row>
+          <Statistic title="Status" value={currentErp.status} />
+        </Row>
+      </PageHeader>
+      <Divider dashed />
+      <Descriptions title={`Client : ${currentErp.client.name}`}>
+        <Descriptions.Item label={translate('Address')}>{client.address}</Descriptions.Item>
+        <Descriptions.Item label={translate('email')}>{client.email}</Descriptions.Item>
+        <Descriptions.Item label={translate('Phone')}>{client.phone}</Descriptions.Item>
+        <Descriptions.Item label={translate('Description')}>
+          {currentErp?.description}
+        </Descriptions.Item>
+        <Descriptions.Item label={translate('Resolution')}>
+          {currentErp?.resolution}
+        </Descriptions.Item>
+      </Descriptions>
+      <Divider />
+      <Row gutter={[12, 0]}>
+        <span>
+          <strong>{translate('Notes')}</strong>
+        </span>
+
+        <Divider />
+      </Row>
+      {notes.map((note) => (
+        <Item key={note._id} item={note} currentErp={currentErp}></Item>
+      ))}
+    </>
+  );
+}
