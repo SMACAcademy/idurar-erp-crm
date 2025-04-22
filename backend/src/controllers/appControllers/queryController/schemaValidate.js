9ab2e8ce66a661ffa869d68b9e108684b3e91ec0
akashk11 <akashvinod777@gmail.com>
2025-04-22 17:53:17 +0400

    add resolution field to query validation schema

--- backend/src/controllers/appControllers/queryController/schemaValidate.js
+++ backend/src/controllers/appControllers/queryController/schemaValidate.js
@@ -3,6 +3,7 @@ const schema = Joi.object({
   client: Joi.alternatives().try(Joi.string(), Joi.object()).required(),
   description: Joi.string().required(),
   number: Joi.number().required(),
+  resolution: Joi.string().optional().max(100),
   status: Joi.string().required().valid('Open', 'InProgress', 'Closed'),
   // array cannot be empty
   notes: Joi.array()
