3f7c6a33935198462c686c32e42ec7af2749b839
sreeman <sreemanlingala333@gmail.com>
2025-06-22 22:34:33 +0530

    formatting code

--- backend/src/models/appModels/Queries.js
+++ backend/src/models/appModels/Queries.js
@@ -1,39 +1,49 @@
-const mongoose=require('mongoose')
-const generateName=()=>{
-    const names=["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah", "Ivan", "Julia"]
-    const random=names[Math.floor(Math.random() * 10)]
-    return random
-}
-const querySchema=mongoose.Schema({
-    customername:{
-        type:String,
-     
-        default:generateName()
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    resolution:{
-        type:String,
-        maxlength:100
-    },
-    status:{
-        type:String,
-        enum:['open','closed','running']
-    },
-    notes:[{
-    description:{
-      type:String,
-      required:true
-    },
-    queryId:{
-      type:mongoose.Schema.Types.ObjectId,
-        ref:'Queries',
-    }
-    }]
+const mongoose = require('mongoose');
+const generateName = () => {
+  const names = [
+    'Alice',
+    'Bob',
+    'Charlie',
+    'Diana',
+    'Ethan',
+    'Fiona',
+    'George',
+    'Hannah',
+    'Ivan',
+    'Julia',
+  ];
+  const random = names[Math.floor(Math.random() * 10)];
+  return random;
+};
+const querySchema = mongoose.Schema({
+  customername: {
+    type: String,
 
-
-  
-})
-module.exports=mongoose.model('Queries',querySchema)
+    default: generateName(),
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  resolution: {
+    type: String,
+    maxlength: 100,
+  },
+  status: {
+    type: String,
+    enum: ['open', 'closed', 'running'],
+  },
+  notes: [
+    {
+      description: {
+        type: String,
+        required: true,
+      },
+      queryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Queries',
+      },
+    },
+  ],
+});
+module.exports = mongoose.model('Queries', querySchema);
